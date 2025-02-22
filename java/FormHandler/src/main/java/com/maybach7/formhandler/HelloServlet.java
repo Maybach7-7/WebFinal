fcf21761f3f497a2730700928266a59cf3a2e7d7
Maybach7 <d.s.samusev@gmail.com>
2025-02-22 23:55:15 +0300

    Make testing launch nginx and tomcat

--- java/FormHandler/src/main/java/com/maybach7/formhandler/HelloServlet.java
+++ java/FormHandler/src/main/java/com/maybach7/formhandler/HelloServlet.java
@@ -1,14 +1,17 @@
 package com.maybach7.formhandler;
 
 import java.io.*;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello World</h1>");
+        out.println("<h1>Hello, World!</h1>");
     }
 }
